91816e3990804262f197301b34b3a5f1b36715c5
Mikkomario <mikko_hilpinen@hotmail.com>
2017-08-03 21:54:20 +0300

    Added transformation to collisionShape

--- src/utopia/conflict/util/CollisionShape.scala
+++ src/utopia/conflict/util/CollisionShape.scala
@@ -3,6 +3,10 @@ package utopia.conflict.util
 import utopia.genesis.util.Circle
 import utopia.genesis.util.Bounds
 import utopia.genesis.util.Vector3D
+import utopia.genesis.util.TransformableShape
+import utopia.genesis.util.Transformation
+import utopia.genesis.util.Angle
+import utopia.genesis.util.Extensions._
 
 /**
  * Collision shapes are used when testing collisions between objects. The shapes may consist of 
@@ -13,9 +17,12 @@ import utopia.genesis.util.Vector3D
  * the shape only consists of circles
  * @param circles The circle(s) that are part of this collision shape. An empty vector if the shape only 
  * consists of a polygon / polygons
+ * @param circleToPolygonEdges the amount of edges used when approximating a circle or an ellipsoid 
+ * with a polygon
  */
 class CollisionShape(val convexPolygons: Vector[Polygon] = Vector(), 
-        val circles: Vector[Circle] = Vector())
+        val circles: Vector[Circle] = Vector(), val circleToPolygonEdges: Int = 12) 
+        extends TransformableShape[CollisionShape]
 {
     // ATTRIBUTES    -------------------------
     
@@ -34,4 +41,33 @@ class CollisionShape(val convexPolygons: Vector[Polygon] = Vector(),
      * multipart shapes
      */
     def isMultiPart = circles.size + convexPolygons.size > 1
+    
+    /**
+     * Converts the shape circle portions to polygons using as many edges as defined in 
+     * <i>circleToPolygonEdges</i>
+     */
+    def circlesAsPolygons = circles.map { circle => Polygon((
+                for { i <- 0 until circleToPolygonEdges } yield Vector3D.lenDir(circle.radius, 
+                new Angle(math.Pi * 2 * i / circleToPolygonEdges))).toVector) }
+    
+    
+    // IMPLEMENTED METHODS    ----------------
+    
+    override def transformedWith(transformation: Transformation) = 
+    {
+        val transformedPolygons = convexPolygons.map { transformation(_) }
+        
+        // Some transformations allow the circles to retain their shape while others will not
+        if ((transformation.shear ~== Vector3D.zero) && (transformation.scaling.x ~== transformation.scaling.y))
+        {
+            val transformedCircles = circles.map { original => 
+                    Circle(transformation(original.origin), original.radius * transformation.scaling.x) }
+            new CollisionShape(transformedPolygons, transformedCircles)
+        }
+        else 
+        {
+            val transformedCirclePolygons = circlesAsPolygons.map { transformation(_) }
+            new CollisionShape(transformedPolygons ++ transformedCirclePolygons)
+        }
+    }
 }
